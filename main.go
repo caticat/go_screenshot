e4c3dc214ec41789fdfea378276ba5d9f53e6e0b
pan <catifish@163.com>
2020-10-22 20:17:45 +0800

    全屏截图例子

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kbinani/screenshot"
+	"image/png"
+	"os"
+)
+
+func main() {
+	fmt.Println(111)
+	simple()
+}
+
+func simple() {
+	n := screenshot.NumActiveDisplays()
+
+	for i := 0; i < n; i++ {
+		bounds := screenshot.GetDisplayBounds(i)
+
+		img, err := screenshot.CaptureRect(bounds)
+		if err != nil {
+			panic(err)
+		}
+		fileName := fmt.Sprintf("%d_%dx%d.png", i, bounds.Dx(), bounds.Dy())
+		file, _ := os.Create(fileName)
+		defer file.Close()
+		png.Encode(file, img)
+
+		fmt.Printf("#%d : %v \"%s\"\n", i, bounds, fileName)
+	}
+}
